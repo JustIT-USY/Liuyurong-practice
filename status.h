1ec9923374b1eb986d2da3bb84f88db435dd56ff
刘玉蓉 <43777384+longme2@users.noreply.github.com>
2018-11-11 17:59:54 +0800

    Create status.h

---
+++ status.h
@@ -0,0 +1,17 @@
+#ifndef STATUS_H_INCLUDED
+#define STATUS_H_INCLUDED
+
+
+typedef int Status;
+
+#define OK 1
+#define ERROR 0
+#define TRUE 1
+#define FALSE 0
+#define OVERFLOW -1
+
+typedef int ElemType;
+
+//typedef Student ElemType;
+
+#endif // STATUS_H_INCLUDED
